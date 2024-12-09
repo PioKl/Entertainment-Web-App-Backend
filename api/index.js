d1d753b79e298141b5eb946635030f29eaf6d092
PioKl <kosiu342@gmail.com>
2024-12-09 17:18:10 +0100

    Update(express, mongoose)

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 /* Dotenv is a zero-dependency module that loads environment variables from a .env file into process.env. */
-require("dotnev").config();
+require("dotenv").config();
 /* ExpressJS allows us to set up middleware to respond to HTTP Requests. */
 const express = require("express");
 /* Mongoose is a MongoDB client library providing object modeling for use in an asynchronous environment. Mongoose supports both promises and callbacks. */
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const userRoutes = require("../routes/auth");
 
-const app = expresss();
+const app = express();
 const port = process.env.PORT || 5000;
 
 // Konfiguracja CORS
@@ -16,8 +16,8 @@ app.use(
   cors({
     //domeny frontendowe
     origin: ["http://localhost:3000"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Dozwolone metody HTTP
+    allowedHeaders: ["Content-Type", "Authorization"], // Dozwolone nagłówki
   })
 );
 
@@ -25,14 +25,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Używanie routera dla ścieżki, na razie będzie /api/auth
-//app.use("/api/auth", userRoutes);
+app.use("/api/auth", userRoutes);
 
 // Połączenie z MongoDB
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+  .catch((err) => {
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1); // Zamknij serwer w przypadku błędu połączenia z bazą danych
+  });
+
+// Endpoint testowy
+app.get("/api/entertainmentData", (req, res) => {
+  res.send("API is working!");
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
